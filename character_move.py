8c8bf21c75fc95d7d63447fb3d683171ee1dcf35
ssnghyxn-k <kimsunghyun8312@gmail.com>
2024-09-24 09:55:08 +0900

    def functioins

--- character_move.py
+++ character_move.py
@@ -1,6 +1,14 @@
 from pico2d import *
 import math
 
+def run_rectangle():
+    print('RECTANGLE')
+    pass
+
+def run_circle():
+    print('CIRCLE')
+    pass
+
 open_canvas()
 
 grass = load_image('grass.png')
